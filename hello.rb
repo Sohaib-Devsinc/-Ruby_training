b0fd75f02afa9e55d822eb4e037afd793200676a
Sohaib Bin Maqsood <sohaib.maqsood@devsinc.com>
2022-03-28 17:24:32 +0500

    Today's Practice

--- hello.rb
+++ hello.rb
@@ -1,3 +1,54 @@
 # frozen_string_literal: true
 
-puts 'Hello World!'
+# name = 'sohaib Bin Maqsood'
+# puts name[0..-2]
+
+# binary_string = '1011'
+# puts binary_string.ljust(10, '1')
+
+# string = 'ruby programming'
+# puts string.start_with? 'ruby'
+
+# arr = %w[a b c]
+# puts arr.join
+
+# puts "a".to_i
+
+# b = <<-STRING
+# aaa
+# bbb
+# ccc
+# STRING
+
+# b = %Q(
+#   aaa
+#   bb
+#   cc
+#   dd
+# )
+# puts b
+
+# str = "this is string"
+# str.freeze
+
+# str.replace("this is new string")
+
+
+# str = "this is string"
+# str.freeze
+# puts str.frozen?
+# str = "this is new string"
+# puts str.frozen?
+
+# foo = :bar
+# puts "symbol #{foo} with object id = #{foo.object_id}"
+# foo = :bar
+# puts "symbol #{foo} with object id = #{foo.object_id}"
+
+
+def introduction(age,gender,*names)
+  "Meet #{names.join(' ')}, who's #{age} and #{gender}"
+end
+
+
+introduction(32,Female, sohaib)
